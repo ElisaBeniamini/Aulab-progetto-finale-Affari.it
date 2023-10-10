12f304fd6cbba9ce093345fc7bc24cc102468965
Emanuele-14 <tommasi.emanuelet@libero.it>
2023-10-10 22:24:58 +0200

    config-provider modified

--- app/View/Components/Dashboard.php
+++ app/View/Components/Dashboard.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class Auth extends Component
+class Dashboard extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class Auth extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('template.auth');
+        return view('template.dashboard');
     }
 }
