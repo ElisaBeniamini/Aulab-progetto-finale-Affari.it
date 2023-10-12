0433bb9458ab22dafd6a526f494277819037a042
Emanuele-14 <tommasi.emanuelet@libero.it>
2023-10-12 18:40:11 +0200

    modifiche nav-fortify

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -20,100 +20,59 @@
         <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
             <li class="nav-item" role="presentation">
                 <a class="nav-link active custom-purple-button" id="tab-login" data-mdb-toggle="pill"
-                    href="#pills-login" role="tab" aria-controls="pills-login" aria-selected="true">Login</a>
+                    href="#pills-login" role="tab" aria-controls="pills-login" aria-selected="true">Entra</a>
             </li>
             <li class="nav-item" role="presentation">
                 <a class="nav-link" id="tab-register" data-mdb-toggle="pill" href="#pills-register" role="tab"
-                    aria-controls="pills-register" aria-selected="false">Register</a>
+                    aria-controls="pills-register" aria-selected="false">Registrati</a>
             </li>
         </ul>
         <!-- Pills navs -->
 
-        <!-- Pills content -->
-        <div class="tab-content">
         <div class="tab-pane show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
-                <form>
-                    <!-- Email input -->
-                    <div class="form-outline mb-4">
-                        <input type="email" id="loginName" class="form-control" />
-                        <label class="form-label" for="loginName">Email or username</label>
-                    </div>
-
-                    <!-- Password input -->
-                    <div class="form-outline mb-4">
-                        <input type="password" id="loginPassword" class="form-control" />
-                        <label class="form-label" for="loginPassword">Password</label>
-                    </div>
-
-                    <!-- 2 column grid layout -->
-                    <div class="row mb-4">
-                        <div class="col-md-6 d-flex justify-content-center">
-                            <!-- Checkbox -->
-                            <div class="form-check mb-3 mb-md-0">
-                                <input class="form-check-input" type="checkbox" value="" id="loginCheck"
-                                    checked />
-                                <label class="form-check-label" for="loginCheck"> Remember me </label>
-                            </div>
-                        </div>
-
-                        <div class="col-md-6 d-flex justify-content-center">
-                            <!-- Simple link -->
-                            <a href="#!">Forgot password?</a>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf <!-- Aggiungi il token CSRF -->
+        
+                <!-- Email input -->
+                <div class="form-outline mb-4">
+                    <input type="email" id="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus />
+                    <label class="form-label" for="email">Email</label>
+                </div>
+        
+                <!-- Password input -->
+                <div class="form-outline mb-4">
+                    <input type="password" id="password" class="form-control" name="password" required />
+                    <label class="form-label" for="password">Password</label>
+                </div>
+        
+                <!-- 2 column grid layout -->
+                <div class="row mb-4">
+                    <div class="col-md-6 d-flex justify-content-center">
+                        <!-- Checkbox -->
+                        <div class="form-check mb-3 mb-md-0">
+                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            <label class="form-check-label" for="remember"> Ricordati di me </label>
                         </div>
                     </div>
-
-                    <!-- Submit button -->
-                    <button type="submit" class="btn custom-purple-button btn-block mb-4">Sign in</button>
-
-                    <!-- Register buttons -->
-                    <div class="text-center">
-                        <p>Not a member? <a href="#!">Register</a></p>
+        
+                    <div class="col-md-6 d-flex justify-content-center">
+                        <!-- Simple link -->
+                        <a href="{{ route('password.request') }}">Ho dimenticato la password</a>
                     </div>
-                </form>
-            </div>
-
-            <div class="tab-pane" id="pills-register" role="tabpanel" aria-labelledby="tab-register">
-
-                <form>
-              
-
-                   <!-- Username input -->
-                    <div class="form-outline mb-4">
-                        <input type="text" id="registerUsername" class="form-control" />
-                        <label class="form-label" for="registerUsername">Username</label>
-                    </div>
-
-                    <!-- Email input -->
-                    <div class="form-outline mb-4">
-                        <input type="email" id="registerEmail" class="form-control" />
-                        <label class="form-label" for="registerEmail">Email</label>
-                    </div>
-
-                    <!-- Password input -->
-                    <div class="form-outline mb-4">
-                        <input type="password" id="registerPassword" class="form-control" />
-                        <label class="form-label" for="registerPassword">Password</label>
-                    </div>
-
-                    <!-- Repeat Password input -->
-                    <div class="form-outline mb-4">
-                        <input type="password" id="registerRepeatPassword" class="form-control" />
-                        <label class="form-label" for="registerRepeatPassword">Repeat password</label>
-                    </div>
-
-                    <!-- Checkbox -->
-                    <div class="form-check d-flex justify-content-center mb-4 custom-checkbox-container">
-                        <input class="form-check-input me-2 custom-checkbox" type="checkbox" value=""
-                            id="registerCheck" checked aria-describedby="registerCheckHelpText" />
-                        <label class="form-check-label" for="registerCheck">
-                            I have read and agree to the terms
-                        </label>
-                    </div>
-
-                    <!-- Submit button -->
-                    <button type="submit" class="btn custom-purple-button btn-block mb-4">Sign in</button>
-                </form>
-            </div>
+                </div>
+        
+                <!-- Submit button -->
+                <button type="submit" class="btn custom-purple-button btn-block mb-4">Registrazione</button>
+        
+                <!-- Register buttons -->
+                <div class="text-center">
+                    <p>Non sei un membro? <a href="{{ route('register') }}">Registrati</a></p>
+                </div>
+            </form>
         </div>
-    </div>
-</div>
+@auth
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <button type="submit" class="btn">Disconnetti</button>
+    </form>
+@endauth
