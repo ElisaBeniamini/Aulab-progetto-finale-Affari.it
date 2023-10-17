68f7bd702f49886cc73e53686f0aa0f78b702645
ElisaBeniamini <elisaaa8783@gmail.com>
2023-10-17 21:58:43 +0200

    inserita img homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,11 +1,16 @@
 <x-layout>
     <x-navbar />
     <!-- Header homepage -->
+    <header>
+        <img class="img-fluid" src="\img\img-header.jpg" style="width: 100%; height: 50%;" alt="shopping">
+    </header>
+
+
+
+
+
     <a class="btn btn-outline-primary btn-inserisci-annuncio" href="{{ route('announcement.index') }}"> Vedi tutti gli
         Annunci</a>
-
-
-
 
 
 
