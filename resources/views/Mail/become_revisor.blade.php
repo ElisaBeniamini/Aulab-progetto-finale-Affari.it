0889f5bc52f29f0ad0e05f137e76361fa906a491
Anna S <annasaracino1998@gmail.com>
2023-10-18 19:49:14 +0200

    modifiche grafiche zona revisore

--- resources/views/Mail/become_revisor.blade.php
+++ resources/views/Mail/become_revisor.blade.php
@@ -1,10 +1,15 @@
 <x-layout><x-navbar/>
-<div>
+<div class="text-center mt-5">
     <h1>Un utente ha richiesto di lavorare con noi.</h1>
     <h2>Ecco i suoi dati</h2>
+    
+    
+    <div class="card-body shadow rounded col-6 mx-auto p-3">
+    <h5 class="card-title">
     <p>Nome: {{$user->name}}</p>
     <p>Email: {{$user->email}}</p>
     <p>Se vuoi renderlo revisore clicca qui:</p>
-    <a href="{{route('make.revisor',compact('user'))}}">Rendi revisore</a>
+    <a href="{{route('make.revisor',compact('user'))}}" class="rendi btn btn-outline-primary">Rendi revisore</a>
+</div></div>
 </div>
 </x-layout>
