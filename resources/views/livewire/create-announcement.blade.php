d4fe8a3981de84e5a29ba65a8e0df7111c22de6e
Anna S <annasaracino1998@gmail.com>
2023-10-19 20:50:17 +0200

    modifiche creazione annuncio

--- resources/views/livewire/create-announcement.blade.php
+++ resources/views/livewire/create-announcement.blade.php
@@ -1,10 +1,11 @@
 <div class="container-fluid mt-5">
     <div class="row">
-        <div class="col-12 col-md-6">
+        <div class="text-center mt-2">
             <h2 class="mb-4 title-real-time">
                 Carica un nuovo Annuncio
             </h2>
             <!-- Start Form inserimento annunci -->
+            <div class="card-body shadow rounded col-6 mx-auto p-3">
             <form wire:submit.prevent="store">
                 @csrf
                 @if (session()->has('announcement'))
@@ -26,31 +27,35 @@
                     <span class="error text-danger">{{ $message }}</span>
                     @enderror
                 </div>
-                <!-- Prezzo -->
-                <div class="mb-3">
-                    <label for="price" class="form-label">Prezzo</label>
-                    <input type="number" class="form-control input-register" id="price" wire:model="price" step="0.01" required>
-                    @error('price')
-                    <span class="error text-danger">{{ $message }}</span>
-                    @enderror
-                </div>
+                
+                <div class="d-flex justify-content-center">
+    <!-- Prezzo -->
+    <div class="mb-3 me-3">
+        <label for="price" class="form-label">Prezzo</label>
+        <input type="number" class="form-control input-register" id="price" wire:model="price" step="0.01" required>
+        @error('price')
+        <span class="error text-danger">{{ $message }}</span>
+        @enderror
+    </div>
 
-                <!--Start-Scelta-Categorie-->
-                <div class="mb-3">
-                    <label for="category" class="form-label">Scegli Categoria</label>
-                    <select wire:model.defer="category" id="category" class="form-control input-register">
-                        @foreach ($categories as $category)
-                        <option value="{{ $category->id }}">{{ $category->name }}</option>
-                        @endforeach
-                    </select>
-                    @error('category')
-                    <span class="error text-danger">{{ $message }}</span>
-                    @enderror
-                </div>
+    <!-- Scegli Categoria -->
+    <div class="mb-3">
+        <label for="category" class="form-label">Scegli Categoria</label>
+        <select wire:model.defer="category" id="category" class="seleziona category form-control input-register">
+            @foreach ($categories as $category)
+            <option value="{{ $category->id }}">{{ $category->name }}</option>
+            @endforeach
+        </select>
+        @error('category')
+        <span class="error text-danger">{{ $message }}</span>
+        @enderror
+    </div>
+</div>
+
                 <!--End-Scelta-Categorie-->
 
                 <!-- Pulsante di invio -->
-                <button type="submit" class="btn custom-purple-button btn-register mt-3 mb-5">Carica articolo</button>
+                <button type="submit" class="carica btn custom-purple-button btn-register mt-3 mb-5">Carica articolo</button>
             </form>
             <!-- End Form inserimento annunci -->
         </div>
