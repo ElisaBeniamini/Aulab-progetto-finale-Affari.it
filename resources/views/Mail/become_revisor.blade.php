827b26e6c97d165bf1ce65e7278d0998917a421c
Anna S <annasaracino1998@gmail.com>
2023-10-21 15:21:23 +0200

    rivedi revisore

--- resources/views/Mail/become_revisor.blade.php
+++ resources/views/Mail/become_revisor.blade.php
@@ -1,15 +1,15 @@
 <x-layout><x-navbar/>
 <div class="text-center mt-5">
-    <h1>Un utente ha richiesto di lavorare con noi.</h1>
-    <h2>Ecco i suoi dati:</h2>
+    <h1>Vuoi lavorare con noi?</h1>
+    <h2>Rivedi i tuoi dati:</h2>
     
     
     <div class="card-body shadow rounded col-6 mx-auto p-3">
     <h5 class="card-title">
     <p>Nome: {{$user->name}}</p>
     <p>Email: {{$user->email}}</p>
-    <p>Se vuoi renderlo revisore clicca qui:</p>
-    <a href="{{route('make.revisor',compact('user'))}}" class="rendi btn btn-outline-primary">Rendi revisore</a>
+    <p>Se vuoi richiedere di diventare revisore clicca qui:</p>
+    <a href="{{route('make.revisor',compact('user'))}}" class="rendi btn btn-outline-primary">Invia richiesta</a>
 </div></div>
 </div>
 </x-layout>
