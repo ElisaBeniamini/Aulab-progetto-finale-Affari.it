0ab3096b1d04e93790d73301b48bcc7fd9111738
danielealato2 <danielealato97@yahoo.com>
2023-10-24 19:52:11 +0200

    modifiche css

--- resources/views/Mail/become_revisor.blade.php
+++ resources/views/Mail/become_revisor.blade.php
@@ -9,8 +9,8 @@
                 <p> {{ __('ui.nome') }}: {{ $user->name }}</p>
                 <p> {{ __('ui.email') }}: {{ $user->email }}</p>
                 <p> {{ __('ui.diventa-revisore-clicca-qui') }}</p>
-                <a href="{{ route('make.revisor', compact('user')) }}"
-                    class="rendi btn btn-outline-primary">{{ __('ui.invia-richiesta') }}</a>
+                <a href="{{ route('make.revisor', compact('user')) }}" class="rendi btn btn-outline-primary">{{ __('ui.invia-richiesta') }}
+                </a>
         </div>
     </div>
 </x-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,13 +5,17 @@
             <div class="row align-items-center">
                 <div class="col-12 col-md-6 d-flex justify-content-center">
                     <div>
-                        <h2 class="title-register">
-                            Iscriviti su PRESTO.IT
-                        </h2>
-                        <p class="paragraf-register pt-3">
-                            Ricevi tutti i vantaggi di un epoca digitale più che mai<br>
-                            scopri di più solla nostra piattaforma...
-                        </p>
+                        <div>
+                            <h2 class="title-register">
+                                Iscriviti su PRESTO.IT
+                            </h2>
+                        </div>
+                        <div>
+                            <p class="paragraf-register pt-3">
+                                Ricevi tutti i vantaggi di un epoca digitale più che mai<br>
+                                scopri di più solla nostra piattaforma...
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div class="col-12 col-md-6 px-5">
@@ -22,51 +26,44 @@
                         <!-- Inserisci Name input -->
                         <div class="form-outline mb-4">
                             <label class="form-label" for="registerEmail">Nome</label>
-                            <input type="text" id="name" name="name"
-                                class="form-control input-register  @error('name') is-invalid @enderror"
-                                value="{{ old('name') }}">
+                            <input type="text" id="name" name="name" class="form-control input-register  @error('name') is-invalid @enderror" value="{{ old('name') }}">
                             @error('name')
-                                <span class="text-danger">{{ $message }}</span>
+                            <span class="text-danger">{{ $message }}</span>
                             @enderror
                         </div>
 
                         <!-- Insersci Email input -->
                         <div class="form-outline mb-4">
                             <label class="form-label" for="registerEmail">Email</label>
-                            <input type="email" id="registerEmail" name="email"
-                                class="form-control input-register @error('email') is-invalid @enderror"
-                                value="{{ old('email') }}" />
+                            <input type="email" id="registerEmail" name="email" class="form-control input-register @error('email') is-invalid @enderror" value="{{ old('email') }}" />
                             @error('email')
-                                <span class="text-danger">{{ $message }}</span>
+                            <span class="text-danger">{{ $message }}</span>
                             @enderror
                         </div>
 
                         <!-- Inserisci Password input -->
                         <div class="form-outline mb-4 input-password-register">
                             <label class="form-label" for="registerPassword">Password</label>
-                            <input type="password" id="registerPassword" name="password"
-                                class="form-control input-register  @error('email') is-invalid @enderror"
-                                value="{{ old('password') }}" placeholder="Password minimo 8 caratteri" />
+                            <input type="password" id="registerPassword" name="password" class="form-control input-register  @error('email') is-invalid @enderror" value="{{ old('password') }}" placeholder="Password minimo 8 caratteri" />
                             @error('password')
-                                <span class="text-danger">{{ $message }}</span>
+                            <span class="text-danger">{{ $message }}</span>
                             @enderror
                         </div>
 
                         <!-- Conferma Password input -->
                         <div class="form-outline mb-4">
                             <label class="form-label" for="registerRepeatPassword">Conferma password</label>
-                            <input type="password" id="registerRepeatPassword" name="password_confirmation"
-                                class="form-control input-register  @error('password_confirmation') is-invalid @enderror"
-                                value="{{ old('password_confirmation') }}" />
+                            <input type="password" id="registerRepeatPassword" name="password_confirmation" class="form-control input-register  @error('password_confirmation') is-invalid @enderror" value="{{ old('password_confirmation') }}" />
                             @error('password')
-                                <span class="text-danger">{{ $message }}</span>
+                            <span class="text-danger">{{ $message }}</span>
                             @enderror
                         </div>
 
                         <!-- Submit button Registrati-->
                         <a href="{{ route('announcement.create') }}">
-                            <button type="submit"
-                                class="btn custom-purple-button btn-block mt-4 btn-register">Registrati</button>
+                            <button type="submit" class="btn custom-purple-button btn-block mt-4 btn-register">
+                                Registrati
+                            </button>
                         </a>
                     </form>
                 </div>
