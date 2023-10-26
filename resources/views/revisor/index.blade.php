d69ad408046180605c1989f0629ccc30f28e59cd
Anna S <annasaracino1998@gmail.com>
2023-10-26 11:50:42 +0200

    modifiche area revisore

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -29,8 +29,36 @@
                         {{ __('ui.pubblicato-da') }} : {{ $announcement_to_check->user->name }} <br>
                         {{ __('ui.il') }} {{ $announcement_to_check->created_at->format('d/m/Y') }}
                     </p>
+
+                    <div class="justify-content-center d-flex">
+                        <form
+                            action="{{ route('revisor.accept_announcement', ['announcement' => $announcement_to_check]) }}"
+                            method="POST">
+                            @csrf
+                            @method('PATCH')
+                            <button type="submit" class="btn btn-success shadow mx-2"> {{ __('ui.accetta') }}</button>
+                        </form>
+                        <form
+                            action="{{ route('revisor.reject_announcement', ['announcement' => $announcement_to_check]) }}"
+                            method="POST">
+                            @csrf
+                            @method('PATCH')
+                            <button type="submit" class="btn btn-danger shadow"> {{ __('ui.rifiuta') }}</button>
+                        </form>
+                    </div>
+
+
                 </div>
             </div>
         </div>
+        </div>
+        </li>
+        </ul>
+        </div>
+        </div>
+        <!-- Add carousel controls here -->
+        </div>
     @endif
+
+
 </x-layout>
