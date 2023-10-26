0bf67dd81fbae30d44aab916227be21dbcee65b8
ElisaBeniamini <elisaaa8783@gmail.com>
2023-10-26 19:32:22 +0200

    risolto revisor img

--- app/Http/Livewire/CreateAnnouncement.php
+++ app/Http/Livewire/CreateAnnouncement.php
@@ -54,13 +54,8 @@ class CreateAnnouncement extends Component
 
     public function store()
     {
+
         $this->validate(); //Se i dai sono validati OK procedi alla creazione.
-        $announcement = new Announcement();
-        if (count($this->images)) {
-            foreach ($this->images as $image) {
-                $announcement->images()->create(['path' => $image->store('images', 'public')]);
-            }
-        }
         $category = Category::find($this->category);
 
         $announcement = $category->announcements()->create([
@@ -69,10 +64,17 @@ class CreateAnnouncement extends Component
             'price' => $this->price,
             'image' => $this->image,
         ]);
+        if (count($this->images)) {
+            foreach ($this->images as $image) {
+                $announcement->images()->create(['path' => $image->store('images', 'public')]);
+            }
+        }
+
+
 
         Auth::user()->announcements()->save($announcement);
 
-        $this->reset('title', 'description', 'price', 'category', 'image'); //Al submit pulisci i campi del form.
+        $this->reset('title', 'description', 'price', 'category', 'image', 'images', 'temporary_images'); //Al submit pulisci i campi del form.
 
         session()->flash('announcement', 'Annuncio creato correttamente, sarà pubblicato dopo la revisione.');
     }
--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -9,54 +9,52 @@
 
     @if ($announcement_to_check)
         <div class="container">
-            <div class="it-carousel-wrapper it-carousel-landscape-abstract-three-cols splide" data-bs-carousel-splide>
-                @if ($announcement_to_check->images)
-                    @foreach($announcement_to_check->images as $image)
-                        <div class="carousel-item @if($loop->first) active @endif">
-                            <img src="{{ Storage::url($image->path) }}" class="img-fluid"/>
+            <div class="row d-flex justify-content-center">
+                <div class="col-5 d-flex justify-content-end">
+                    @if ($announcement_to_check->images)
+                        @foreach ($announcement_to_check->images as $image)
+                            <div class=" @if ($loop->first) active @endif">
+                                <img src="{{ Storage::url($image->path) }}" class="img-fluid shadow rounded" />
+                            </div>
+                        @endforeach
+                    @endif
+                </div>
+                <!-- card dati utente -->
+                <div class="col-7 d-flex justify-content-start shadow rounded">
+                    <div class="card-body  col-6 mx-auto p-3 d-flex align-items-center flex-column mt-3">
+                        <h5 class="card-title">
+                            {{ __('ui.titolo') }}: {{ $announcement_to_check->title }}
+                        </h5>
+                        <p class="card-text">
+                            {{ __('ui.descrizione') }}: {{ $announcement_to_check->body }}
+                        </p>
+                        <p class="card-footer">
+                            {{ __('ui.pubblicato-da') }} : {{ $announcement_to_check->user->name }} <br>
+                            {{ __('ui.il') }} {{ $announcement_to_check->created_at->format('d/m/Y') }}
+                        </p>
+
+                        <div class="justify-content-center d-flex">
+                            <form
+                                action="{{ route('revisor.accept_announcement', ['announcement' => $announcement_to_check]) }}"
+                                method="POST">
+                                @csrf
+                                @method('PATCH')
+                                <button type="submit" class="btn btn-success shadow mx-2">
+                                    {{ __('ui.accetta') }}</button>
+                            </form>
+                            <form
+                                action="{{ route('revisor.reject_announcement', ['announcement' => $announcement_to_check]) }}"
+                                method="POST">
+                                @csrf
+                                @method('PATCH')
+                                <button type="submit" class="btn btn-danger shadow"> {{ __('ui.rifiuta') }}</button>
+                            </form>
                         </div>
-                    @endforeach
-                @endif
 
-                <div class="card-body shadow rounded col-6 mx-auto p-3 text-center mt-3">
-                    <h5 class="card-title">
-                        {{ __('ui.titolo') }}: {{ $announcement_to_check->title }}
-                    </h5>
-                    <p class="card-text">
-                        {{ __('ui.descrizione') }}: {{ $announcement_to_check->body }}
-                    </p>
-                    <p class="card-footer">
-                        {{ __('ui.pubblicato-da') }} : {{ $announcement_to_check->user->name }} <br>
-                        {{ __('ui.il') }} {{ $announcement_to_check->created_at->format('d/m/Y') }}
-                    </p>
 
-                    <div class="justify-content-center d-flex">
-                        <form
-                            action="{{ route('revisor.accept_announcement', ['announcement' => $announcement_to_check]) }}"
-                            method="POST">
-                            @csrf
-                            @method('PATCH')
-                            <button type="submit" class="btn btn-success shadow mx-2"> {{ __('ui.accetta') }}</button>
-                        </form>
-                        <form
-                            action="{{ route('revisor.reject_announcement', ['announcement' => $announcement_to_check]) }}"
-                            method="POST">
-                            @csrf
-                            @method('PATCH')
-                            <button type="submit" class="btn btn-danger shadow"> {{ __('ui.rifiuta') }}</button>
-                        </form>
                     </div>
-
-
                 </div>
             </div>
-        </div>
-        </div>
-        </li>
-        </ul>
-        </div>
-        </div>
-        <!-- Add carousel controls here -->
         </div>
     @endif
 
