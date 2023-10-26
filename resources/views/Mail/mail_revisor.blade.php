3e339df2a9d048a7a2988607a491db6a92c1d308
danielealato2 <danielealato97@yahoo.com>
2023-10-26 18:55:28 +0200

    modifica mail

--- resources/views/Mail/mail_revisor.blade.php
+++ resources/views/Mail/mail_revisor.blade.php
@@ -1,4 +1,4 @@
-<x-layout><x-navbar />
+<x-layout>
     <div class="text-center mt-5">
         <h1> {{ __('ui.questo-utente-vuole-lavorare-con-noi') }}</h1>
         <h2> {{ __('ui.ecco-i-suoi-dati') }}</h2>
@@ -9,8 +9,10 @@
                 <p> {{ __('ui.nome') }}: {{ $user->name }}</p>
                 <p> {{ __('ui.email') }}: {{ $user->email }}</p>
                 <p> {{ __('ui.rendilo-revisore-e-clicca-qui') }}</p>
-                <a href="{{ route('make.revisor', compact('user')) }}" class="rendi btn btn-outline-primary">{{ __('ui.accetta') }}
+                <a href="{{ route('make.revisor', compact('user')) }}" class="rendi btn btn-outline-primary">
+                    {{ __('ui.accetta') }}
                 </a>
+            </h5>
         </div>
     </div>
 </x-layout>
